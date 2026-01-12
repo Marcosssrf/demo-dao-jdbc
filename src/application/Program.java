6c76fc3f47acf91de1047e1dd6f04b96fab37d11
Marcosssrf <marcosssrf@hotmail.com>
2026-01-11 21:43:54 -0300

    FindById, FindByDepartment

--- src/application/Program.java
+++ src/application/Program.java
@@ -6,17 +6,24 @@ import model.entities.Department;
 import model.entities.Seller;
 
 import java.util.Date;
+import java.util.List;
 
 public class Program {
 	public static void main(String[] args) {
 
-		Department obj = new Department(1, "Books");
-		Seller seller = new Seller(21, "Bob", "bob@gmail.com", new Date(), 3000.0, obj);
-
 		SellerDao sellerDao = new DaoFactory().createSellerDao();
 
-		System.out.println(obj);
+		System.out.println("=== TEST 1: seller findById ===");
+		Seller seller = sellerDao.findById(3);
 		System.out.println(seller);
+
+		System.out.println();
+		System.out.println("=== TEST 2: seller findByDepartment ===");
+		Department department = new Department(2, null);
+		List<Seller> sellers = sellerDao.findByDepartment(department);
+		for (Seller obj : sellers) {
+			System.out.println(obj);
+		}
 
 	}
 }
