95eaa8b7580d15723a8d1349f6a86b5f682e5307
Marcosssrf <marcosssrf@hotmail.com>
2026-01-11 22:26:02 -0300

    SellerDaoJDBC com todas funções + DepartmentDaoJDBC com todas funções + Program2 com teste do DepartmentDao

--- src/application/Program.java
+++ src/application/Program.java
@@ -7,10 +7,12 @@ import model.entities.Seller;
 
 import java.util.Date;
 import java.util.List;
+import java.util.Scanner;
 
 public class Program {
 	public static void main(String[] args) {
 
+		Scanner sc = new Scanner(System.in);
 		SellerDao sellerDao = new DaoFactory().createSellerDao();
 
 		System.out.println("=== TEST 1: seller findById ===");
@@ -32,6 +34,26 @@ public class Program {
 			System.out.println(obj);
 		}
 
+		System.out.println();
+		System.out.println("=== TEST 4: seller insert ===");
+		Seller newSeller = new Seller(null, "Greg", "greg@gmail.com", new Date(), 4000.0, department);
+		sellerDao.insert(newSeller);
+		System.out.println("Inserted! New id = "+newSeller.getId());
+
+		System.out.println();
+		System.out.println("=== TEST 5: seller update ===");
+		seller = sellerDao.findById(1);
+		seller.setName("Martha Waine");
+		sellerDao.update(seller);
+		System.out.println("Update compleated!");
+
+		System.out.println();
+		System.out.println("=== TEST 6: seller delete ===");
+		System.out.println("Enter id for delete test: ");
+		int id = sc.nextInt();
+		sellerDao.deleteById(id);
+		System.out.println("Delete compleated!");
+		sc.close();
 
 	}
 }
